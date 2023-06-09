b70c03e22cc6a48eadd23f59dafed4e75c151eac
Anthony Moreno <anthony.moreno.cisnero@gmail.com>
2023-06-09 10:46:10 -0400

    Se actualiza el entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,18 +1,21 @@
 #!/bin/sh -l
 
+#Variables Java
 JAVA_HOME='/opt/jdk-13.0.1'
 PATH="$JAVA_HOME/bin:$PATH"
 export PATH
+#Variables Mvn
 M2_HOME='/opt/apache-maven-3.6.3'
 PATH="$M2_HOME/bin:$PATH"
 export PATH
+#Variables Axways
 AXWAY_APIM_CLI_HOME='apim-cli-1.13.7'
 export AXWAY_APIM_CLI_HOME
 PATH="$AXWAY_APIM_CLI_HOME/scripts:$PATH"
 export PATH
 alias apim=apim.sh
-
-chmod -R 777 /entrypoint.sh
+alias
+#comandos
 java -version
 mvn -version
 apim api get -a /api/v1/sample-anthony -s prd8252
